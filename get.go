82845da7a04f13b20d51f90f52f9209a6e4a1d33
Matt Robinson <brimstone@the.narro.ws>
2016-05-24 21:40:15 -0400

    Adding GetRaw, GetInt, and making insert work correctly.

--- get.go
+++ get.go
@@ -16,3 +16,30 @@ func (t *Tskv) GetSince(when time.Time) *Tskv {
 	}
 	return toreturn
 }
+
+// GetRaw returns an raw repssentation of the series
+func (t *Tskv) GetRaw() []Element {
+	t.cleanup()
+	var all []Element
+	current := time.Now().Truncate(t.frequency)
+	oldest := current.Add(-t.duration)
+	for current.After(oldest) {
+		all = append(all, t.elements[current])
+		current = current.Add(-t.frequency)
+	}
+	return all
+}
+
+// GetInt returns and integer representation of the series
+func (t *Tskv) GetInt() []int {
+	var all []int
+	for _, raw := range t.GetRaw() {
+		if raw.Value() == nil {
+			all = append(all, 0)
+		} else {
+			all = append(all, raw.Int())
+		}
+
+	}
+	return all
+}
